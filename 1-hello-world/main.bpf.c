7408251e6bdf3d0e0a94cf2567e40560a08f7567
unknown <james.yang516@outlook.com>
2022-06-22 20:57:44 +0800

    impove make

--- 1-hello-world/main.bpf.c
+++ 1-hello-world/main.bpf.c
@@ -6,13 +6,14 @@
 SEC("kprobe/do_sys_openat2")
 int kprobe__do_sys_openat2(struct pt_regs *ctx)
 {
-		char file_name[256];
-		bpf_probe_read(file_name, sizeof(file_name), PT_REGS_PARM2(ctx));
-
-		char fmt[] = "open file %s\n";
-		bpf_trace_printk(fmt, sizeof(fmt), &file_name);
-
-		return 0;
+	char file_name[256];
+	bpf_probe_read(file_name, sizeof(file_name), PT_REGS_PARM2(ctx));
+	
+	char fmt[] = "open file %s\n";
+	bpf_trace_printk(fmt, sizeof(fmt), &file_name);
+	
+	return 0;
+	
 }
 
 char _license[] SEC("license") = "GPL";
