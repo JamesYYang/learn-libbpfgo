d68c44e9faa5900196de232f1f99032641c5dce2
unknown <james.yang516@outlook.com>
2022-06-26 11:04:50 +0800

    add tracepoint example

---
+++ 4-tracepoint/main.go
@@ -0,0 +1,92 @@
+package main
+
+import "C"
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	bpf "github.com/aquasecurity/libbpfgo"
+)
+
+type eData struct {
+	Pid      uint32
+	Filename string
+}
+
+func resizeMap(module *bpf.Module, name string, size uint32) error {
+	m, err := module.GetMap(name)
+	if err != nil {
+		return err
+	}
+	if err = m.Resize(size); err != nil {
+		return err
+	}
+	if actual := m.GetMaxEntries(); actual != size {
+		return fmt.Errorf("map resize failed, expected %v, actual %v", size, actual)
+	}
+	return nil
+}
+
+func main() {
+	log.SetPrefix(fmt.Sprintf("[%d]: ", os.Getpid()))
+	log.SetFlags(log.Ldate | log.Lmicroseconds)
+
+	log.Println("load bpf file...")
+
+	bpfModule, err := bpf.NewModuleFromFile("main.bpf.o")
+	if err != nil {
+		panic(err)
+	}
+	defer bpfModule.Close()
+
+	if err = resizeMap(bpfModule, "events", 8192); err != nil {
+		panic(err)
+	}
+
+	if err := bpfModule.BPFLoadObject(); err != nil {
+		panic(err)
+	}
+	prog, err := bpfModule.GetProgram("tracepoint_openat")
+	if err != nil {
+		panic(err)
+	}
+	if _, err := prog.AttachTracepoint("syscalls", "sys_enter_openat"); err != nil {
+		panic(err)
+	}
+
+	eventsChannel := make(chan []byte)
+	pb, err := bpfModule.InitRingBuf("events", eventsChannel)
+	if err != nil {
+		panic(err)
+	}
+
+	pb.Start()
+	defer func() {
+		pb.Stop()
+		pb.Close()
+	}()
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	for {
+		select {
+		case e := <-eventsChannel:
+			pid := binary.LittleEndian.Uint32(e[0:4])
+			filename := string(bytes.TrimRight(e[4:], "\x00")) // Remove excess 0's from comm, treat as string
+			ed := eData{
+				Pid:      pid,
+				Filename: filename,
+			}
+			log.Printf("pid[%d] %v", ed.Pid, ed.Filename)
+		case <-c:
+			log.Fatal("program interrupted")
+			break
+		}
+	}
+}
